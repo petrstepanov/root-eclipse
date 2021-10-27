6a885f14b31e1e6cd867d730804e09cc1c0f8a0c
Petr Stepanov <stepanovps@gmail.com>
2021-10-26 22:39:19 -0400

    Added event loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
-#ifndef __CINT__
-#include <RooGlobalFunc.h>
-#endif
-
+#include <TApplication.h>
 #include <TCanvas.h>
 #include <TH1.h>
 
-void myFunc(){
-  ;
-}
-
-int main() {
+// Paste your ROOT script code here
+void yourRootScritCode(){
 	TH1* hist = new TH1D("hist", "hist", 10, 0, 10);
 	hist->FillRandom("gaus",10000);
-	hist->Draw();	
-	myFunc();
+	hist->Draw();
+}
+
+// This code is for standalone build. Not visible for ROOT interpreter,
+#ifndef __CINT__
+
+int main(int argc, char **argv) {
+	// Instantiate TApplication
+	TApplication* app = new TApplication("rootEclipse", &argc, argv);
+
+	// Execute your ROOT script
+	yourRootScritCode();
+
+	// Enter the event loop
+	app->Run();
+
+	// Return success
 	return 0;
 }
+
+#endif
